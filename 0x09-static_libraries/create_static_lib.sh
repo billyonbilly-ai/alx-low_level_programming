fa17af3ab54cfa7468c7c4b835d6f130897c1f5d
billyonbilly-ai <billyonbilly.work@gmail.com>
2023-01-10 15:19:36 -0800

    task 1

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# Create an empty list of object files
-objects=""
+gcc -Wall -pedantic -Werror -Wextra -c *.c
 
-# Iterate over all .c files in the current directory
-for c_file in *.c; do
-  # Compile the .c file into an object file
-  gcc -c $c_file
-  # Add the object file to the list
-  objects="$objects $(basename $c_file .c).o"
-done
+ar -rc liball.a *.o
 
-# Create the static library from the object files
-ar rcs liball.a $objects
-
+ranlib liball.a
