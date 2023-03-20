4aa11a49b0cb22a0634d66ac8fbea9b5027ca021
billyonbilly-ai <billyonbilly.work@gmail.com>
2023-03-20 16:54:41 -0700

    Dynamic libraries

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,39 @@
+#include "main.h"
+<<<<<<< HEAD
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always EXIT_SUCCESS.
+ *
+ *     */
+
+int main(void)
+
+{
+
+	    printf("%d\n", _strlen("My Dyn Lib"));
+
+	        return (EXIT_SUCCESS);
+
+=======
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return (EXIT_SUCCESS);
+>>>>>>> 026221c606cd1e711417b2521f85704813c1ab61
+}
